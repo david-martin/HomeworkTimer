38ba11b340e100539aa6e701fd8b090b8c0e6830
David Martin <david.martin@redhat.com>
2018-02-10 23:04:46 +0000

    Add history screen, shows history of finished timers (saved on device)

---
+++ app/src/main/java/com/example/dmartin/homeworktimer/HistoryActivity.java
@@ -0,0 +1,89 @@
+package com.example.dmartin.homeworktimer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+import com.couchbase.lite.DatabaseOptions;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.Emitter;
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.Mapper;
+import com.couchbase.lite.Query;
+import com.couchbase.lite.QueryEnumerator;
+import com.couchbase.lite.QueryRow;
+import com.couchbase.lite.View;
+import com.couchbase.lite.android.AndroidContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    private Manager manager;
+    private Database database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO: Why is the toolbar/title missing at the top of this screen?
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        try {
+            startDB();
+        } catch (Exception e) {
+            // TODO: More graceful way to handle db exceptions
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        View viewItemsByDate = database.getView("history");
+
+        // TODO: Is this kind of view mapping even needed? Is there a simple query that would suffice?
+        // TODO: Sort by created date, newest first
+        viewItemsByDate.setMap(new Mapper() {
+            @Override
+            public void map(Map<String, Object> document, Emitter emitter) {
+                Object createdAt = document.get("created_at");
+                if (createdAt != null) {
+                    emitter.emit(createdAt.toString(), null);
+                }
+            }
+        }, "1.0");
+        List<String> items = new ArrayList<>();
+        Query query = viewItemsByDate.createQuery();
+        QueryEnumerator queryResults = null;
+        try {
+            queryResults = query.run();
+        } catch (CouchbaseLiteException e) {
+            // TODO: More graceful way to handle db exceptions
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (queryResults.hasNext()) {
+            QueryRow row = queryResults.next();
+            Document doc = row.getDocument();
+            // TODO: Nicer formatting of history rows
+            items.add(String.format(" %s - %s - %s", doc.getId(), doc.getProperty("text"), doc.getProperty("created_at")));
+        }
+
+        ListAdapter historyListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+
+        ListView historyListView = findViewById(R.id.historyListView);
+        historyListView.setAdapter(historyListAdapter);
+    }
+
+    // TODO: Share DB manager & connection between activities?
+    private void startDB() throws Exception {
+        manager = new Manager(new AndroidContext(getApplicationContext()), Manager.DEFAULT_OPTIONS);
+        DatabaseOptions options = new DatabaseOptions();
+        options.setCreate(true);
+        database = manager.openDatabase("history", options);
+    }
+
+}
--- app/src/main/java/com/example/dmartin/homeworktimer/MainActivity.java
+++ app/src/main/java/com/example/dmartin/homeworktimer/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.dmartin.homeworktimer;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+import com.couchbase.lite.DatabaseOptions;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.android.AndroidContext;
 import com.google.common.base.Stopwatch;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Manager manager;
+    private Database database;
 
     private TextView timerTextView;
     private Stopwatch stopwatch = Stopwatch.createUnstarted();
@@ -39,59 +55,104 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            startDB();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
         timerTextView = findViewById(R.id.timerTextView);
 
         Button timerButton = findViewById(R.id.timerButton);
         timerButton.setText("start");
-        timerButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Button finishButton = findViewById(R.id.finishButton);
-                finishButton.setEnabled(true);
-                Button timerButton = (Button) v;
-                if (timerButton.getText().equals("pause")) {
-                    stopwatch.stop();
-                    timerHandler.removeCallbacks(timerRunnable);
-                    timerButton.setText("resume");
-                } else {
-                    stopwatch.start();
-                    timerHandler.postDelayed(timerRunnable, 0);
-                    timerButton.setText("pause");
-                }
+        timerButton.setOnClickListener((view) -> {
+            Button finishButton = findViewById(R.id.finishButton);
+            finishButton.setEnabled(true);
+            if (timerButton.getText().equals("pause")) {
+                stopwatch.stop();
+                timerHandler.removeCallbacks(timerRunnable);
+                timerButton.setText("resume");
+            } else {
+                stopwatch.start();
+                timerHandler.postDelayed(timerRunnable, 0);
+                timerButton.setText("pause");
             }
+        });
+
+        Button historyButton = findViewById(R.id.historyButton);
+        historyButton.setOnClickListener((view) -> {
+            Intent historyIntent = new Intent(this, HistoryActivity.class);
+            startActivity(historyIntent);
         });
 
         Button finishButton = findViewById(R.id.finishButton);
         finishButton.setText("finish");
         finishButton.setEnabled(false);
-        finishButton.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                if (stopwatch.isRunning()) {
-                    stopwatch.stop();
-                }
-                long millis = stopwatch.elapsed(TimeUnit.MILLISECONDS);
-                stopwatch.reset(); // OK to reset now that we've taken a reading
-                timerHandler.removeCallbacks(timerRunnable);
-                Button timerButton = findViewById(R.id.timerButton);
-                timerButton.setText("start");
-
-                TextView timerPreviousView = findViewById(R.id.timerPreviousView);
-                int seconds = (int) (millis / 1000);
-                int minutes = seconds / 60;
-                seconds = seconds % 60;
-                millis = millis % 1000;
-                timerPreviousView.setText(String.format("%02d:%02d:%03d", minutes, seconds, millis));
-
-                TextView timerTextView = findViewById(R.id.timerTextView);
-                timerTextView.setText(String.format("%02d:%02d:%03d", 0, 0, 0));
-
-                Button finishButton = (Button) view;
-                finishButton.setEnabled(false);
+        finishButton.setOnClickListener((android.view.View view) -> {
+            if (stopwatch.isRunning()) {
+                stopwatch.stop();
             }
+            long millis = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+            stopwatch.reset(); // OK to reset now that we've taken a reading
+            timerHandler.removeCallbacks(timerRunnable);
+            timerButton.setText("start");
+
+            TextView timerPreviousView = findViewById(R.id.timerPreviousView);
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            millis = millis % 1000;
+            String previousValue = String.format("%02d:%02d:%03d", minutes, seconds, millis);
+            timerPreviousView.setText(previousValue);
+            try {
+                createHistoryRecord(previousValue);
+            } catch (CouchbaseLiteException e) {
+                // TODO: More graceful way to handle db exceptions
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+
+            TextView timerTextView = findViewById(R.id.timerTextView);
+            timerTextView.setText(String.format("%02d:%02d:%03d", 0, 0, 0));
+
+            Button button = (Button) view;
+            button.setEnabled(false);
         });
+    }
+
+    private Document createHistoryRecord(String value) throws CouchbaseLiteException {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+        UUID uuid = UUID.randomUUID();
+        Calendar calendar = GregorianCalendar.getInstance();
+        long currentTime = calendar.getTimeInMillis();
+        String currentTimeString = dateFormatter.format(calendar.getTime());
+
+        String id = currentTime + "-" + uuid.toString();
+
+        Document document = database.createDocument();
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("_id", id);
+        // TODO: store raw history value instead of formatted string
+        properties.put("text", value);
+        properties.put("created_at", currentTimeString);
+        document.putProperties(properties);
+
+        Log.d("app", String.format("Created new value with id: %s", document.getId()));
+
+        return document;
+
+    }
+
+    // TODO: Share DB manager & connection between activities?
+    private void startDB() throws Exception {
+        manager = new Manager(new AndroidContext(getApplicationContext()), Manager.DEFAULT_OPTIONS);
+        DatabaseOptions options = new DatabaseOptions();
+        options.setCreate(true);
+        database = manager.openDatabase("history", options);
     }
 
     @Override
